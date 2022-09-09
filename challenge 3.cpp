67867d5738581a2d6274f1e1541a485b0d2b2149
faissal17 <faissalaoukacha@gmail.com>
2022-09-09 12:34:36 +0100

    test7

---
+++ challenge 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a , b ;
+	printf("entre la premier valeur :");
+	scanf("%d" ,&a);
+	 
+	 printf("entre la deuxieme valeur :");
+	 scanf("%d",&b);
+	 
+	 printf("la somme de deux valeur est %d", a+b);
+	 
+	return 0;
+}

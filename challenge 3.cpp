6bbc39ef5f5d9d04ab8c54292cee6cb3c004d4bb
faissal17 <112889901+faissal17@users.noreply.github.com>
2022-09-10 20:34:52 +0100

    Update challenge 3.cpp

--- challenge 3.cpp
+++ challenge 3.cpp
@@ -3,6 +3,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	int main(int argc, char** argv) {
 	int a , b ;
 	printf("entre la premier valeur :");
 	scanf("%d" ,&a);
@@ -10,7 +11,12 @@ int main(int argc, char** argv) {
 	 printf("entre la deuxieme valeur :");
 	 scanf("%d",&b);
 	 
+	 if (a == b) {
+	 printf (" le triple est %d" , (a+b)*3);
+	 }
+	 else {
 	 printf("la somme de deux valeur est %d", a+b);
+    }
 	 
 	return 0;
 }
